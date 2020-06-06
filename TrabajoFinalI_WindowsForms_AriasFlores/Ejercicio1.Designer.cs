b061522a09de5ca6e5ee53ffcc7edff465eade2c
Jose Arias <joseluisariasflores01@gmail.com>
2020-06-06 09:12:27 -0500

    Trabajo Final E1 v1

---
+++ TrabajoFinalI_WindowsForms_AriasFlores/Ejercicio1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace TrabajoFinalI_WindowsForms_AriasFlores
+{
+    partial class Ejercicio1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbhora = new System.Windows.Forms.Label();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbhora
+            // 
+            this.lbhora.AutoSize = true;
+            this.lbhora.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbhora.Location = new System.Drawing.Point(166, 105);
+            this.lbhora.Name = "lbhora";
+            this.lbhora.Size = new System.Drawing.Size(209, 45);
+            this.lbhora.TabIndex = 0;
+            this.lbhora.Text = "00:00:00:000";
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btniniciar.Location = new System.Drawing.Point(12, 105);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(139, 33);
+            this.btniniciar.TabIndex = 3;
+            this.btniniciar.Text = "INICIAR";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            this.btniniciar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btniniciar_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(381, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "DETENER";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRONOMETRO";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(191, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 33);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "PARAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 208);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btniniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbhora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbhora;
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TrabajoFinalI_WindowsForms_AriasFlores/Ejercicio1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics; //stopwtch
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoFinalI_WindowsForms_AriasFlores
+{
+    public partial class Ejercicio1 : Form
+    {
+        int milisegundos=0;
+        int segundos;
+        int minutos;
+        int horas;
+        public Ejercicio1()
+        {
+            InitializeComponent();
+        }
+        int i;
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+           
+                timer1.Enabled=true;
+           
+           
+            
+        }
+        Stopwatch stopWatch = new Stopwatch();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stopWatch.Start();
+            if (stopWatch.IsRunning)
+            {
+             
+
+                TimeSpan ts = stopWatch.Elapsed;
+                lbhora.Text = String.Format("{0:00}:{1:00}:{2:00}:{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 1);
+
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+         //   stopWatch.Stop();
+            lbhora.Text = "00:00:00:00";
+            stopWatch.Reset();
+          //  btnLap.Visible = false;
+        }
+
+        private void btniniciar_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
